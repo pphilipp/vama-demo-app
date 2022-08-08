b188aa64c8761da52da2e87d312124b8e5c28230
😎pylypbuhaievskyi <philipp@eatwoodspoon.com>
2022-08-08 22:22:50 +0300

    Update: WIP: transparent animated toolbar

--- app/src/main/java/com/app/vama/presentation/MainActivity.kt
+++ app/src/main/java/com/app/vama/presentation/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.app.vama.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.app.vama.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
     }
 }
