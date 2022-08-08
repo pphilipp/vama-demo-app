d1a804a86d77151cdcb6649fe742ac12ddffd7ea
😎pylypbuhaievskyi <philipp@eatwoodspoon.com>
2022-08-08 23:06:40 +0300

    Update: NetworkModule.kt

--- app/src/main/java/com/app/vama/VamaApplication.kt
+++ app/src/main/java/com/app/vama/VamaApplication.kt
@@ -2,6 +2,7 @@ package com.app.vama
 
 import android.app.Application
 import com.app.vama.di.appModule
+import com.app.vama.di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
@@ -18,7 +19,7 @@ class VamaApplication: Application() {
         startKoin {
             androidLogger()
             androidContext(this@VamaApplication)
-            modules(appModule)
+            modules(appModule + networkModule)
         }
     }
 }
---
+++ app/src/main/java/com/app/vama/data/remote/ApiService.kt
@@ -0,0 +1,6 @@
+package com.app.vama.data.remote
+
+interface ApiService {
+//    https://rss.applemarketingtools.com/api/v2/  us/music/most-played/10/albums.json
+
+}
